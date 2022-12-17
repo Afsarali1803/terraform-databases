183146ce954befdd1522d4755b1ebfde8af06ea6
= <=>
2022-12-17 06:39:18 +0530

    project

--- main.tf
+++ main.tf
@@ -6,3 +6,8 @@ module "docdb" {
     source              = "./vendor/modules/docdb"  
     ENV                 = var.ENV 
 }
+
+module "redis" {
+    source              = "./vendor/modules/redis"  
+    ENV                 = var.ENV 
+}
